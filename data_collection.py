9e6b5dd4a89acfe8714540fc29f0e525bf8a51bb
John <jpicken12@gmail.com>
2020-07-28 11:03:58 -0400

    I dont know what is going on

--- data_collection.py
+++ data_collection.py
@@ -9,10 +9,7 @@ path = r"G:\coding\salary_proj/chromedriver"
 df = gs.get_jobs('data scientist', 15, False, path)
 
 print(df.iloc[0])
-<<<<<<< HEAD
 
 data_path = r"G:\coding\salary_proj"
 
 df.to_csv(path = data_path, index = False)
-=======
->>>>>>> d6904845e72118e27196b4ecbb9b8dc17fbebb87
